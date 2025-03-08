b47df6e52e81dff417840112cb90625819f7954c
deepmavani <mavanideep46@gmail.com>
2025-03-08 15:30:16 +0530

    Xcountries

---
+++ src/Countries.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const Countries = () => {
+ 
+ const [countries,setCountries] =useState ([]);
+
+ useEffect(() => {
+  axios
+  .get("https://xcountries-backend.azurewebsites.net/all")
+  .then((res)=>{
+    setCountries(res.data);
+  })
+  .catch((err)=>{
+    console.error("error Fetching the Data",err);
+  })
+ } , [])
+
+return (
+  <div style={styles.container}>
+    {countries.map((country,index)=>(
+        <CountryCard key={index} name={country.name} flag={country.flag}/>
+    ))}
+  </div>
+)
+
+}
+
+
+const styles = {
+  container: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    gap: "20px",
+    padding: "20px",
+  },
+  card: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    border: "1px solid black",
+    borderRadius: "10px",
+    padding: "10px",
+    height: "200px",
+    width: "200px",
+    boxShadow: "2px 2px 10px rgba(0, 0, 0, 0.1)",
+    backgroundColor: "#f9f9f9",
+  },
+  flag: {
+    width: "100px",
+    height: "100px",
+    objectFit: "cover",
+    borderRadius: "5px",
+  },
+  name: {
+    marginTop: "10px",
+    fontSize: "16px",
+    fontWeight: "bold",
+  },
+};
+
+export default Countries;
